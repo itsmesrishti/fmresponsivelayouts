c442eaca32ad71c3ab19a39fe148a901133d3819
Srishti Chaudhary <srishticho@gmail.com>
2024-09-07 11:47:01 +0530

    mobile design done

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,14 +1,17 @@
 const shareBtn = document.getElementById("share-btn");
 const shareBlock = document.getElementById("share-block");
 const authorInfo = document.getElementById("author-info");
+const cardFooter = document.getElementById("card-footer");
 
 shareBtn.addEventListener("click", (event) => {
   event.stopPropagation();
-  shareBlock.style.display = "block";
+  shareBlock.style.display = "flex";
   authorInfo.style.display = "none";
+  cardFooter.classList.add("dark-bg");
 
   document.body.addEventListener("click", () => {
     shareBlock.style.display = "none";
     authorInfo.style.display = "flex";
+    cardFooter.classList.remove("dark-bg");
   });
 });
